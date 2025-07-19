5814bb3436a2faf4592f53eec49c33b468ae0609
yathigsaas <yath.yiv@igsaas.asia>
2025-07-19 19:50:16 +0700

    first commit amqp project with spring boot project

---
+++ src/main/java/com/yathdev/amqp_spring_boot/AmqpSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.yathdev.amqp_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yathdev/amqp_spring_boot/AmqpSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yathdev.amqp_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
